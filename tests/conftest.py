76b70741304bddb5ee142bd7a81ca1bf942724ee
albert-garreta <mtdenm@gmail.com>
2022-08-08 16:57:49 +0200

    first commit

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+import asyncio
+import os
+import pytest
+from starkware.starknet.testing.starknet import Starknet
+# Some test fixtures are defined here
+
+CONTRACT = os.path.join("contracts", "contract.cairo")
+
+
+
+
+@pytest.fixture(scope="module")
+def event_loop():
+    return asyncio.new_event_loop()
+
+
+@pytest.fixture(scope="module")
+async def starknet_factory(contracts):
+    starknet = await Starknet.empty()
+    return starknet
+
+
+@pytest.fixture(scope="module")
+async def test_contract(starknet_factory):
+    starknet = starknet_factory
+    # Deploy the account contract
+    contract = await starknet.deploy(source=CONTRACT)
+    return contract
